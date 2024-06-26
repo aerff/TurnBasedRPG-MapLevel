d096891f08cb944182e5f9dc2b633f46061b65eb
abdunzelil <mstfocak123@gmail.com>
2024-06-26 18:46:34 +0300

    Buton ve Yorum (MO)
    Ana menünün başla butonunu biraz yukarı kaydırdım ve main menu scriptine iki tane yorum satırı ekledim

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
+# Mustafa Ocak - sorun yok
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/character_selection.tscn")
+# Mustafa Ocak - hiçbir şey yapılmadı
