bcd04bb269c9bad334f9d9f5f6fb3c65a6d0487d
abdunzelil <mstfocak123@gmail.com>
2024-07-02 22:06:11 +0300

    2 Temmuz Updates
    Coin mechanic (incomplete)
    Gold mechanic (incomplete)
    Button specialization (complete

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,18 +6,16 @@ var boss_heal = 0
 var boss = null
 var boss_path
 var level
-
-	
-# 0, 2, 4... çift sayı ise -> Boss'un turu
-# 1, 3, 5... tek sayı ise -> Bizim turumuz
+@onready var button_1 = $Button
+@onready var button_2 = $Button2
+@onready var button_3 = $Button3
+@onready var button_4 = $Button4
 
 func _ready(): # --> Oyun başlangıcında ayarlanacak şeyler
 	$PauseScreen.visible = false
 	$Boss/Spider.visible = false
 	$Boss/VampireBoss.visible = false
-	$TextureButton.disabled = false
-	$TextureButton2.disabled = false
-	$coinlabel.text = str(Global.coin)
+	# Boss seçimini handle ediyoruz
 	if Boss.which_boss == 1:
 		level = 1
 		boss_path = $Boss/Spider
@@ -30,12 +28,25 @@ func _ready(): # --> Oyun başlangıcında ayarlanacak şeyler
 		boss_path.visible = true
 		boss_damage = 10
 		boss_heal = 3
+	# Karakter seçimini handle ediyoruz
 	if CharacterSelection.choice == 1:
 		$Player/Sprite2D.texture = preload("res://assets/elementalist.png")
+		button_1.text = "Blast"
+		button_2.text = "Fireball"
+		button_3.text = "Heal"
+		button_4.text = "Mana Regen"
 	elif CharacterSelection.choice == 2:
 		$Player/Sprite2D.texture = preload("res://assets/knight.png")
+		button_1.text = "Slash"
+		button_2.text = "Heavy Attack"
+		button_3.text = "Heal"
+		button_4.text = "Mana Regen"
 	elif CharacterSelection.choice == 3:
 		$Player/Sprite2D.texture = preload("res://assets/death.png")
+		button_1.text = "Drain"
+		button_2.text = "Death Blow"
+		button_3.text = "Heal"
+		button_4.text = "Mana Regen"
 	$ColorRect.visible = false
 	toggle_buttons()
 	$TurnLabel.visible = false
@@ -74,13 +85,11 @@ func toggle_buttons():
 		$Button2.disabled = true
 		$Button3.disabled = true
 		$Button4.disabled = true
-		$Button5.disabled = true
 	else: # Basamıyorsam hepsini disabledlıktan çıkar
 		$Button.disabled = false
 		$Button2.disabled = false
 		$Button3.disabled = false
 		$Button4.disabled = false
-		$Button5.disabled = false
 
 func show_turn_label_text(text: String):
 	$TurnLabel.visible = true
@@ -90,58 +99,75 @@ func show_turn_label_text(text: String):
 	$TurnLabel.visible = false
 
 func _on_button_pressed(): # Slash butonuna basıldığında
-	$Player/AnimationPlayer.play("deal_damage")
-	boss_path.get_node("AnimationPlayer").play("take_damage")
-	boss_path.get_node("BossHPBar").value -= 200
-	if boss_path.get_node("BossHPBar").value <= 0:
-		if level in CompletedLevels.completed_levels: # Daha önce kestik
-			Global.coin += 250
-			$coinlabel.text = str(Global.coin)
-		else: # İlk defa kesiyorsak
-			CompletedLevels.completed_levels.append(level)
-			Global.coin += 500
-			$coinlabel.text = str(Global.coin)
-		get_tree().change_scene_to_file("res://scenes/map.tscn")
-	turn += 1
-	toggle_buttons()
-	$Button5.disabled = false
-	
+	var mana_requirement = 0
+	if CharacterSelection.choice == 1: # PYROMANCER
+		mana_requirement = 10
+	elif CharacterSelection.choice == 3: # DEATH
+		mana_requirement = 15
+	if $Player/PlayerManaBar.value < mana_requirement:
+		pass
+	else:
+		$Player/AnimationPlayer.play("deal_damage")
+		boss_path.get_node("AnimationPlayer").play("take_damage")
+		# CONDITIONAL STATEMENTS FOR SPECIFIC CHARACTERS
+		if CharacterSelection.choice == 1: # PYROMANCER
+			fireblast()
+		elif CharacterSelection.choice == 2: # KNIGHT
+			slash()
+		elif CharacterSelection.choice == 3: # DEATH
+			drain()
+		# CONDITIONAL STATEMENTS FOR SPECIFIC CHARACTERS
+		if boss_path.get_node("BossHPBar").value <= 0:
+			if level in CompletedLevels.completed_levels: # Daha önce kestik
+				pass
+			else: # İlk defa kesiyorsak
+				CompletedLevels.completed_levels.append(level)
+				Map.gold_count_as_integer += 50
+			get_tree().change_scene_to_file("res://scenes/map.tscn")
+		turn += 1
+		toggle_buttons()
+		play_turn()
 
 func _on_button_2_pressed(): # Fireball butonuna basıldığında
-	if $Player/PlayerManaBar.value < 15:
+	var mana_requirement = 0
+	if CharacterSelection.choice == 1: # PYROMANCER
+		mana_requirement = 10
+	elif CharacterSelection.choice == 3: # DEATH
+		mana_requirement = 15
+	if $Player/PlayerManaBar.value < mana_requirement:
 		pass
 	else: # Yeterli manamız varsa
 		$Player/AnimationPlayer.play("deal_damage")
 		boss_path.get_node("AnimationPlayer").play("take_damage")
-		boss_path.get_node("BossHPBar").value -= 15
-		$Player/PlayerManaBar.value -= 15
+		# CONDITIONAL STATEMENTS FOR SPECIFIC CHARACTERS
+		if CharacterSelection.choice == 1: # PYROMANCER
+			fireball()
+		elif CharacterSelection.choice == 2: # KNIGHT
+			heavy_attack()
+		elif CharacterSelection.choice == 3: # DEATH
+			death_blow()
+		# CONDITIONAL STATEMENTS FOR SPECIFIC CHARACTERS
 		if boss_path.get_node("BossHPBar").value <= 0:
 			if level in CompletedLevels.completed_levels: # Daha önce kestik
-				Global.coin += 250
-				$coinlabel.text = str(Global.coin)
+				pass
 			else: # İlk defa kesiyorsak
-				Global.coin += 500
-				$coinlabel.text = str(Global.coin)
 				CompletedLevels.completed_levels.append(level)
 			get_tree().change_scene_to_file("res://scenes/map.tscn")
 		turn += 1
 		toggle_buttons()
-		$Button5.disabled = false
-		
+		play_turn()
 
 func _on_button_3_pressed(): # Heal butonuna basıldığında
 	$Player/PlayerHPBar.value += 10
 	turn += 1
 	toggle_buttons()
-	$Button5.disabled = false
-	
+	play_turn()
 
-func _on_button_4_pressed():
+func _on_button_4_pressed(): # Mana butonuna basıldığında
 	$Player/PlayerManaBar.value = $Player/PlayerManaBar.max_value
 	turn += 1
 	toggle_buttons()
-	$Button5.disabled = false
-	
+	play_turn()
 
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
@@ -161,17 +187,24 @@ func _on_main_menu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
+# KNIGHT CHARACTER SKILLS
+func slash(): # Button 1 for the Knight character
+	boss_path.get_node("BossHPBar").value -= 200
+func heavy_attack():
+	boss_path.get_node("BossHPBar").value -= 15
 
-func _on_button_5_pressed(): #Tur bitirme buttonu
-	$Button5.disabled = true
-	play_turn()
+# PYROMANCER CHARACTER SKILLS
+func fireblast(): # Button 1 for the Pyromancer character
+	$Player/PlayerManaBar.value -= 5
+	boss_path.get_node("BossHPBar").value -= 10
+func fireball():	
+	$Player/PlayerManaBar.value -= 10
+	boss_path.get_node("BossHPBar").value -= 20
 
-
-func _on_texture_button_pressed():
-	$Player/PlayerHPBar.value += $Player/PlayerHPBar.max_value
-	$TextureButton.disabled = true
-
-
-func _on_texture_button_2_pressed():
-	$Player/PlayerManaBar.value = $Player/PlayerManaBar.max_value
-	$TextureButton2.disabled = true
+# DEATH CHARACTER SKILLS
+func drain(): # Button 1 for the Death character
+	$Player/PlayerManaBar.value -= 8
+	boss_path.get_node("BossHPBar").value -= 15
+func death_blow():	
+	$Player/PlayerManaBar.value -= 15
+	boss_path.get_node("BossHPBar").value -= 25
